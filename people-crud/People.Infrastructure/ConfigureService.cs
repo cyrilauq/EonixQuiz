ada5bd7a315c5ddc98244625a64f0d8583474ae4
Cyril Auquier <cyrilauquier@hotmail.fr>
2025-01-08 18:37:20 +0100

    inject repository

--- people-crud/People.Infrastructure/ConfigureService.cs
+++ people-crud/People.Infrastructure/ConfigureService.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using People.Domain.Repositories;
 using People.Infrastructure.Data;
+using People.Infrastructure.Repositories;
 
 namespace People.Infrastructure
 {
@@ -13,6 +15,15 @@ namespace People.Infrastructure
             {
                 options.UseSqlite(configuration.GetConnectionString("DefaultConnection") ?? throw new Exception("No connection string named [DefaultConnection] found"));
             });
+
+            services.AddRepositories();
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IPersonRepository, PersonEFRepository>();
 
             return services;
         }
